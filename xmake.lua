a06062b9f7e5f5983702d18321eb973ef0e5af2b
GreyElaina <GreyElaina@outlook.com>
2025-09-01 21:02:41 +0800

    fix: update Python dependency to enable shared configuration

--- xmake.lua
+++ xmake.lua
@@ -53,7 +53,8 @@ package_end()
 
 add_requires("litehtml_local", "pango", "cairo")
 set_languages("c++17")
-add_requires("python", { version = "3.10.x" })
+
+add_requires("python", { version = "3.10.x", configs = {shared = true} })
 add_requireconfs("**.python", { override = true, version = "3.10.x", headeronly = true })
 
 function require_htmlkit()
